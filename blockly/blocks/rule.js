3f556f55f293c35966b549e10031ff83213ecf98
rahul <rahul150795@gmail.com>
2017-06-07 15:06:07 +0530

    first commit

---
+++ blockly/blocks/rule.js
@@ -0,0 +1,290 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Colour blocks for Blockly.
+ *
+ * This file is scraped to extract a .json file of block definitions. The array
+ * passed to defineBlocksWithJsonArray(..) must be strict JSON: double quotes
+ * only, no outside references, no functions, no trailing commas, etc. The one
+ * exception is end-of-line comments, which the scraper will remove.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.rule');  // Deprecated
+goog.provide('Blockly.Constants.Rule');
+
+goog.require('Blockly.Blocks');
+
+
+/**
+ * Common HSV hue for all blocks in this category.
+ * This should be the same as Blockly.Msg.COLOUR_HUE.
+ * @readonly
+ */
+Blockly.Constants.Rule.HUE = 20;
+/** @deprecated Use Blockly.Constants.Colour.HUE */
+Blockly.Blocks.rule.HUE = Blockly.Constants.Rule.HUE;
+
+Blockly.defineBlocksWithJsonArray(
+
+[{
+  "type": "set_category",
+  "message0": "Set %1 discount %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "Category 1",
+          "1"
+        ],
+        [
+          "Category 2",
+          "2"
+        ],
+        [
+          "Category 3",
+          "3"
+        ]
+      ]
+    },
+    {
+      "type": "field_number",
+      "name": "discount",
+      "value": 0
+    }
+  ],
+  "inputsInline": false,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "set_rule",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "p_category",
+      "options": [
+        [
+          "category 1",
+          "1"
+        ],
+        [
+          "category 2",
+          "2"
+        ],
+        [
+          "category 3",
+          "3"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": false,
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "product_name",
+  "message0": "Product %1",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "p_name",
+      "text": " name"
+    }
+  ],
+  "inputsInline": false,
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "product_price",
+  "message0": "Product Price %1",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "p_price",
+      "value": 100
+    }
+  ],
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "product_category",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "category 1",
+          "1"
+        ],
+        [
+          "category 2",
+          "2"
+        ],
+        [
+          "category 3",
+          "3"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": false,
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "p_off",
+  "message0": "%1 %% off ",
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "NAME",
+      "value": 0
+    }
+  ],
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "payment",
+  "message0": "Payment %1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "option",
+      "options": [
+        [
+          "COD",
+          "1"
+        ],
+        [
+          "Online",
+          "2"
+        ]
+      ]
+    }
+  ],
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "cart_item",
+  "message0": "Add Item in cart %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "cart"
+    }
+  ],
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "set_catalog_rule",
+  "message0": "set Catalog Rule %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "c_discount"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "cart_rules",
+  "message0": "Set Cart Rule %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "cart_rule"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 315,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "multi_cond",
+  "message0": "Rule 1 %1 Rule 2 %2",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "con1"
+    },
+    {
+      "type": "input_statement",
+      "name": "con2"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 180,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "sum_block",
+  "message0": "sum %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "sum"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}]
+);
---
+++ blockly/generators/javascript/rule.js
@@ -0,0 +1,120 @@
+/**
+ * @license
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Generating JavaScript for list blocks.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+goog.provide('Blockly.JavaScript.rule');
+
+goog.require('Blockly.JavaScript');
+
+
+
+Blockly.JavaScript['product'] = function(block) {
+  var value_p_name = Blockly.JavaScript.valueToCode(block, 'p_name', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_p_price = Blockly.JavaScript.valueToCode(block, 'p_price', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_p_category = Blockly.JavaScript.valueToCode(block, 'p_category', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['product_name'] = function(block) {
+  var text_p_name = block.getFieldValue('p_name');
+  // TODO: Assemble JavaScript into code variable.
+  var code = text_p_name;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['product_price'] = function(block) {
+  var number_p_price = block.getFieldValue('p_price');
+  // TODO: Assemble JavaScript into code variable.
+  var code = number_p_price;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['product_category'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = dropdown_name;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.JavaScript['set_rule'] = function(block) {
+  var dropdown_p_category = block.getFieldValue('p_category');
+  // TODO: Assemble JavaScript into code variable.
+  var code = dropdown_p_category;
+  // TODO: Chang ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.JavaScript['p_off'] = function(block) {
+  var number_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = number_name;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['payment'] = function(block) {
+  var dropdown_option = block.getFieldValue('option');
+  // TODO: Assemble JavaScript into code variable.
+  var code = dropdown_option;
+  return code;
+};
+
+Blockly.JavaScript['cart_item'] = function(block) {
+  var statements_cart = Blockly.JavaScript.statementToCode(block, 'cart');
+  // TODO: Assemble JavaScript into code variable.
+  var code = statements_cart;
+  return code;
+};
+Blockly.JavaScript['set_catalog_rule'] = function(block) {
+  var statements_c_discount = Blockly.JavaScript.statementToCode(block, 'c_discount');
+  // TODO: Assemble JavaScript into code variable.
+  var code = statements_c_discount;
+  return code;
+};
+
+Blockly.JavaScript['cart_rules'] = function(block) {
+  var statements_cart_rule = Blockly.JavaScript.statementToCode(block, 'cart_rule');
+  // TODO: Assemble JavaScript into code variable.
+  var code = statements_cart_rule;
+  return code;
+};
+
+Blockly.JavaScript['multi_cond'] = function(block) {
+  var statements_con1 = Blockly.JavaScript.statementToCode(block, 'con1');
+  var statements_con2 = Blockly.JavaScript.statementToCode(block, 'con2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = statements_con1+statements_con2;
+  return code;
+};
+
+Blockly.JavaScript['sum_block'] = function(block) {
+  var statements_sum = Blockly.JavaScript.statementToCode(block, 'sum');
+  // TODO: Assemble JavaScript into code variable.
+  var code = statements_sum;
+  return code;
+};
